9c6fee45a30009c2dc41466993e9e63d4c61cc5d
mitch <robertsmj1138@gmail.com>
2020-03-21 11:48:27 -0400

    simple sphere render

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,65 @@
+#include "ray.h"
 #include "vec3.h"
 
+#include <fstream>
 #include <iostream>
+
+vec3 color(const ray& r);
+bool hit_sphere(const vec3& center, float radius, const ray& r);
 
 int main() {
   int nx = 200;
   int ny = 100;
 
+  auto out_file_path = getenv("OUT_FILE_PATH");
+  std::cout << "Opening " << out_file_path << "\n";
+
+  std::ofstream out_file;
+  out_file.open(out_file_path);
+
   // Write ppm file header
-  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  out_file << "P3\n" << nx << " " << ny << "\n255\n";
+
+  vec3 lower_left_corner(-2.0, -1.0, -1.0);
+  vec3 horizontal(4.0, 0.0, 0.0);
+  vec3 vertical(0.0, 2.0, 0.0);
+  vec3 origin(0.0, 0.0, 0.0);
 
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-      // float r = float(i) / float(nx);
-      // float g = float(j) / float(ny);
-      // float b = 0.2;
+      float u = float(i) / float(nx);
+      float v = float(j) / float(ny);
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      vec3 col = color(r);
 
       int ir = int(255.99 * col.r());
       int ig = int(255.99 * col.g());
       int ib = int(255.99 * col.b());
 
-      std::cout << ir << " " << ig << " " << ib << "\n";
+      out_file << ir << " " << ig << " " << ib << "\n";
     }
   }
 
+  out_file.close();
+
   return 0;
+}
+
+// lerp WRT the y-axis
+// t=1.0 => blue
+// t=0.0 => white
+vec3 color(const ray& r) {
+  if (hit_sphere(vec3(0, 0, -1), 0.5, r)) return vec3(1, 0, 0);
+  vec3 unit_direction = unit_vector(r.direction());
+  float t = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0 * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius * radius;
+  float discriminant = b * b - 4 * a * c;
+  return (discriminant > 0);
 }
