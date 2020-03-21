6dc3f554073dca224037c59f80f6e40e85252f4a
mitch <robertsmj1138@gmail.com>
2020-03-21 13:18:37 -0400

    hittables

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
-#include "ray.h"
-#include "vec3.h"
+#include "float.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <fstream>
 #include <iostream>
 
-vec3 color(const ray& r);
-bool hit_sphere(const vec3& center, float radius, const ray& r);
+vec3 color(const ray& r, hittable* world);
+float hit_sphere(const vec3& center, float radius, const ray& r);
 
 int main() {
   int nx = 200;
@@ -25,12 +26,19 @@ int main() {
   vec3 vertical(0.0, 2.0, 0.0);
   vec3 origin(0.0, 0.0, 0.0);
 
+  hittable* list[2];
+  list[0] = new sphere(vec3(0, 0, -1), 0.5);
+  list[1] = new sphere(vec3(0, -100.5, -1), 100);
+  hittable* world = new hittable_list(list, 2);
+
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       float u = float(i) / float(nx);
       float v = float(j) / float(ny);
       ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-      vec3 col = color(r);
+
+      vec3 p = r.point_at_parameter(2.0);
+      vec3 col = color(r, world);
 
       int ir = int(255.99 * col.r());
       int ig = int(255.99 * col.g());
@@ -41,25 +49,40 @@ int main() {
   }
 
   out_file.close();
+  delete world;
+  delete list[0];
+  delete list[1];
+  delete *list;
 
   return 0;
 }
 
-// lerp WRT the y-axis
-// t=1.0 => blue
-// t=0.0 => white
-vec3 color(const ray& r) {
-  if (hit_sphere(vec3(0, 0, -1), 0.5, r)) return vec3(1, 0, 0);
-  vec3 unit_direction = unit_vector(r.direction());
-  float t = 0.5 * (unit_direction.y() + 1.0);
-  return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+vec3 color(const ray& r, hittable* world) {
+  hit_record rec;
+  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+    return 0.5 *
+           vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+  } else {
+    // lerp WRT the y-axis
+    // t=1.0 => blue
+    // t=0.0 => white
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+  }
 }
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+// Detect ray intersections with a sphere
+float hit_sphere(const vec3& center, float radius, const ray& r) {
   vec3 oc = r.origin() - center;
   float a = dot(r.direction(), r.direction());
   float b = 2.0 * dot(oc, r.direction());
   float c = dot(oc, oc) - radius * radius;
   float discriminant = b * b - 4 * a * c;
-  return (discriminant > 0);
+
+  if (discriminant < 0) {
+    return -1;
+  } else {
+    return (-b - sqrt(discriminant)) / (2 * a);
+  }
 }
--- src/ray.h
+++ src/ray.h
@@ -5,13 +5,13 @@
 class ray {
  public:
   ray() {}
-  ray(const vec3& a, const vec3& b) : A(a), B(b) {}
-  vec3 origin() const { return A; }
-  vec3 direction() const { return B; }
-  vec3 point_at_parameter(float t) const { return A + t * B; }
+  ray(const vec3& a, const vec3& b) : orig(a), dir(b) {}
+  vec3 origin() const { return orig; }
+  vec3 direction() const { return dir; }
+  vec3 point_at_parameter(float t) const { return orig + t * dir; }
 
-  vec3 A;
-  vec3 B;
+  vec3 orig;
+  vec3 dir;
 };
 
 #endif
